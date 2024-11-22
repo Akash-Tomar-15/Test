4a20ca327267c56fd218267b2eff644c20e1b084
Akash Tomar <42638735+Akash-Tomar-15@users.noreply.github.com>
2024-11-22 12:26:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Define a route for the root (home) page
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'This is a Test Web Page on Azure Web Services\nBy Akash Tomar !'
 
 # Run the app if this script is executed
 if __name__ == '__main__':
